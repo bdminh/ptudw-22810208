4e35f8b24eb61e9749d2b48b7aa80f452a5f27b3
Minh Bui <minhbuiduy.khtn@gmail.com>
2024-09-12 20:17:01 -0700

    Bài 15

--- controller/indexController.js
+++ controller/indexController.js
@@ -4,6 +4,16 @@ const controller = {};
 const models = require('../models');
 
 controller.showHomePage = async (req, res) => {
+    const categories = await models.Category.findAll();
+    // [1, 2, 3, 4] => [[1], [3, 4], [2]]
+    const secondArray = categories.splice(2, 2);
+    const thirdArray = categories.splice(1, 1);
+    res.locals.categoryArray = [
+        categories,
+        secondArray,
+        thirdArray
+    ];
+    
     const Brand = models.Brand;
     const brands = await Brand.findAll();
     res.render('index', { brands: brands});
