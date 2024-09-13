3a39a84f1553f317a4d0c3412eed8bf302b1136f
Minh Bui <minhbuiduy.khtn@gmail.com>
2024-09-13 17:56:37 +0700

    Bài 18

---
+++ routes/productsRouter.js
@@ -0,0 +1,9 @@
+'use strict';
+
+let express = require('express');
+let router = express.Router();
+let controller = require('../controller/productsController');
+
+router.get('/', controller.show);
+
+module.exports = router;
