ce88abd48c0823781d5b4eb2465205833136ce2f
Minh Bui <minhbuiduy.khtn@gmail.com>
2024-09-23 16:22:42 +0700

    Bài 23

--- controller/productsController.js
+++ controller/productsController.js
@@ -1,6 +1,8 @@
 let controller = {};
 const { attachment } = require('express/lib/response');
 const models = require('../models');
+const sequelize = require('sequelize');
+const Op = sequelize.Op;
 
 controller.getData = async (req, res, next) => {
     let categories = await models.Category.findAll({
@@ -27,6 +29,7 @@ controller.show = async (req, res) => {
     let category = isNaN(req.query.category) ? 0 : parseInt(req.query.category);
     let brand = isNaN(req.query.brand) ? 0 : parseInt(req.query.brand);
     let tag = isNaN(req.query.tag) ? 0 : parseInt(req.query.tag);
+    let keyword = req.query.keyword || '';
 
     let options = {
         attributes: ['id', 'name', 'imagePath', 'stars', 'price', 'oldPrice'],
@@ -43,6 +46,11 @@ controller.show = async (req, res) => {
             model: models.Tag,
             where: { id: tag }
         }];
+    }
+    if (keyword.trim() != '') {
+        options.where.name = {
+            [Op.iLike]: `%${keyword}%`
+        }
     }
 
     let products = await models.Product.findAll(options);
