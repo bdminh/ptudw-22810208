863fafefe7c035c2dfcf64434ee2cf7e75327a51
Minh Bui <minhbuiduy.khtn@gmail.com>
2024-09-27 17:19:59 +0700

    Bài 29

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -6,6 +6,7 @@ let controller = require('../controller/productsController');
 let cartController = require('../controller/cartController');
 
 router.get('/', controller.getData, controller.show);
+router.get('/cart', cartController.show);
 router.get('/:id', controller.getData, controller.showDetails);
 router.post('/cart', cartController.add);
 
