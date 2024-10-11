26df297bb839f64b174597e299cb450f978a72eb
Minh Bui <minhbuiduy.khtn@gmail.com>
2024-10-11 13:50:03 +0700

    31 Xây dựng chức năng thanh toán COD

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -5,5 +5,5 @@ const router = express.Router();
 const controller = require('../controller/usersController');
 
 router.get('/checkout', controller.checkout);
-
+router.post('/placeorders', controller.placeorders);
 module.exports = router;
